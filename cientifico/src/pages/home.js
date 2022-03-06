5da8014aba1e176d700dfbf081846b41220bf37c
Robert Machado <Robert.20.ven@gmail.com>
2022-03-06 01:11:33 -0500

    configuracion de webpack completa

---
+++ cientifico/src/pages/home.js
@@ -0,0 +1,15 @@
+const home = () => {
+    const view = `
+    <div class="Characters">
+        <aticle class="Character-item" >
+            <a href="#/1/" >
+                <img src="image" alt="name">
+                    <h2></h2>
+            </a>
+        </article>
+    </div>
+    `;
+    return view;
+}
+
+export default home;
